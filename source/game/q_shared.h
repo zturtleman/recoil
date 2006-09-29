712d9df7c9cf6dbe9320ab7b4fb05bdd8d1c573a
Discoloda <Discoloda@c1450f78-a31b-0410-b7d4-1f03561c8efe>
2006-09-29 23:24:35 +0000

    getting closer to cross-compiling

--- source/game/q_shared.h
+++ source/game/q_shared.h
@@ -32,28 +32,29 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #define MAX_TEAMNAME 32
 
 #ifdef _WIN32
-
-#pragma warning(disable : 4018)     // signed/unsigned mismatch
-#pragma warning(disable : 4032)
-#pragma warning(disable : 4051)
-#pragma warning(disable : 4057)		// slightly different base types
-#pragma warning(disable : 4100)		// unreferenced formal parameter
-#pragma warning(disable : 4115)
-#pragma warning(disable : 4125)		// decimal digit terminates octal escape sequence
-#pragma warning(disable : 4127)		// conditional expression is constant
-#pragma warning(disable : 4136)
-#pragma warning(disable : 4152)		// nonstandard extension, function/data pointer conversion in expression
-//#pragma warning(disable : 4201)
-//#pragma warning(disable : 4214)
-#pragma warning(disable : 4244)
-#pragma warning(disable : 4142)		// benign redefinition
-//#pragma warning(disable : 4305)		// truncation from const double to float
-//#pragma warning(disable : 4310)		// cast truncates constant value
-//#pragma warning(disable:  4505) 	// unreferenced local function has been removed
-#pragma warning(disable : 4514)
-#pragma warning(disable : 4702)		// unreachable code
-#pragma warning(disable : 4711)		// selected for automatic inline expansion
-#pragma warning(disable : 4220)		// varargs matches remaining parameters
+#   if defined(MSVC)
+#       pragma warning(disable : 4018)     // signed/unsigned mismatch
+#       pragma warning(disable : 4032)
+#       pragma warning(disable : 4051)
+#       pragma warning(disable : 4057)		// slightly different base types
+#       pragma warning(disable : 4100)		// unreferenced formal parameter
+#       pragma warning(disable : 4115)
+#       pragma warning(disable : 4125)		// decimal digit terminates octal escape sequence
+#       pragma warning(disable : 4127)		// conditional expression is constant
+#       pragma warning(disable : 4136)
+#       pragma warning(disable : 4152)		// nonstandard extension, function/data pointer conversion in expression
+#       pragma warning(disable : 4201)
+#       pragma warning(disable : 4214)
+#       pragma warning(disable : 4244)
+#       pragma warning(disable : 4142)		// benign redefinition
+#       pragma warning(disable : 4305)		// truncation from const double to float
+#       pragma warning(disable : 4310)		// cast truncates constant value
+#       pragma warning(disable:  4505) 	// unreferenced local function has been removed
+#       pragma warning(disable : 4514)
+#       pragma warning(disable : 4702)		// unreachable code
+#       pragma warning(disable : 4711)		// selected for automatic inline expansion
+#       pragma warning(disable : 4220)		// varargs matches remaining parameters
+#   endif
 #endif
 
 #include <assert.h>
@@ -93,6 +94,8 @@ short   ShortSwap (short l);
 int		LongSwap (int l);
 float	FloatSwap (const float *f);
 
+#define	CPUSTRING "i386"
+
 //======================= WIN32 DEFINES =================================
 
 #ifdef WIN32
@@ -105,14 +108,18 @@ float	FloatSwap (const float *f);
 // buildstring will be incorporated into the version string
 #ifdef NDEBUG
 #ifdef _M_IX86
+#undef CPUSTRING
 #define	CPUSTRING	"win-x86"
 #elif defined _M_ALPHA
+#undef CPUSTRING
 #define	CPUSTRING	"win-AXP"
 #endif
 #else
 #ifdef _M_IX86
+#undef CPUSTRING
 #define	CPUSTRING	"win-x86-debug"
 #elif defined _M_ALPHA
+#undef CPUSTRING
 #define	CPUSTRING	"win-AXP-debug"
 #endif
 #endif
@@ -126,12 +133,12 @@ static ID_INLINE short BigShort( short l)
 #define LittleShort
 static ID_INLINE int BigLong(int l)
 {
-    LongSwap(l);
+    return LongSwap(l);
 }
 #define LittleLong
 static ID_INLINE float BigFloat(const float *l)
 {
-    FloatSwap(l);
+    return FloatSwap(l);
 }
 #define LittleFloat
 
@@ -1494,8 +1501,6 @@ typedef enum _flag_status {
     FLAG_TAKEN_BLUE,	// One Flag CTF
     FLAG_DROPPED
 } flagStatus_t;
-
-
 
 #define	MAX_GLOBAL_SERVERS				4096
 #define	MAX_OTHER_SERVERS					128
